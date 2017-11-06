dc3793fea60621fc490923041658b1b3a63eed49
Hieu Hoang <hieuhoang@gmail.com>
2017-11-06 14:27:23 +0000

    timings

--- src/amun/gpu/dl4mt/decoder.h
+++ src/amun/gpu/dl4mt/decoder.h
@@ -298,9 +298,9 @@ class Decoder {
           Prod(Probs, T1, *w4);
           PAUSE_TIMER("GetProbs.Prod4");
 
-          //BEGIN_TIMER("GetProbs.BroadcastVec");
+          BEGIN_TIMER("GetProbs.BroadcastVec");
           BroadcastVec(_1 + _2, Probs, *b4);
-          //PAUSE_TIMER("GetProbs.BroadcastVec");
+          PAUSE_TIMER("GetProbs.BroadcastVec");
 
           BEGIN_TIMER("GetProbs.LogSoftMax");
           mblas::LogSoftmax(Probs);
